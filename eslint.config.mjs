04f1bbb3aeaf0f2338f2fef5b6548b844f4649e8
User <esipov@sv-en.ru>
2024-10-14 14:05:16 +0300

    сделан build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
+  {
+    ignores: ["dist/main.js", "src/README.md"],
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
