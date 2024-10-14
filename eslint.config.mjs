ccfe3d933b473afae206a65a81c818406b0728ae
User <esipov@sv-en.ru>
2024-10-14 14:06:47 +0300

    сделан build_2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from "@eslint/js";
 
 export default [
   {
-    ignores: ["dist/main.js", "src/README.md"],
+    ignores: ["docs/main.js", "src/README.md"],
   },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
